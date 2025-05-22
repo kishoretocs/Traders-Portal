2c387e4bf64db558de0d5e5f191309109e9c4e48
kishoretocs <kishoretocs@gmail.com>
2025-05-22 13:51:00 +0530

    final

--- companies/tasks.py
+++ companies/tasks.py
@@ -1,3 +1,4 @@
+
 import csv
 import requests
 from io import StringIO
--- config/test.js
+++
@@ -1,6 +0,0 @@
-import { getAuth } from "firebase/auth";
-
-const auth = getAuth();
-const user = auth.currentUser;                     // Current signed‑in user
-const idToken = await user.getIdToken(/* forceRefresh */ true);
-console.log(idToken);
